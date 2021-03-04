c75b5e275afc7646bfafdd5ecb6d2d269a0efd79
Dipak Kumar Pandey <diapk@tabsquare.com>
2021-03-04 23:51:28 +0530

    Removed unused code.

--- ECommerceApp/ECommerce/Views/Home/HomeView.swift
+++ ECommerceApp/ECommerce/Views/Home/HomeView.swift
@@ -50,39 +50,6 @@ struct HomeView: View {
     
 }
 
-private extension HomeView {
-    
-    var shareView: some View {
-       
-        VStack(alignment: .leading) {
-            Text(viewModel.sharing?.title ?? "")
-                .font(.custom(Constants.AppFont.mediumFont, size: 14))
-                .foregroundColor(.darkBlack)
-                .padding(.bottom, 4)
-
-            Text(viewModel.sharing?.description ?? "")
-                .font(.custom(Constants.AppFont.regularFont, size: 13))
-                .foregroundColor(.warmGrey)
-                .padding(.bottom, Constants.padding)
-
-            HStack() {
-                Text(viewModel.sharing?.shareLink ?? "")
-                    .font(.custom(Constants.AppFont.mediumFont, size: 14))
-                    .foregroundColor(.dustyOrange)
-                
-                Spacer()
-                
-                Image.init(Constants.Images.whatsupShareIcon)
-            }
-
-        }
-        .padding(.all, 12)
-        .background(Color.defaultWhite)
-
-    }
-    
-}
-
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
